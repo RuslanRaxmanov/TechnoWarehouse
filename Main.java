5af04ab5bd57fb9cf12126a8d265d51f3bb9627a
RuslanRaxmanov <milleniumeye17@gmail.com>
2024-05-07 21:29:02 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+import db.Dbinit;
+
+public class Main {
+    public static void main(String[] args) {
+        Dbinit.init();
+    }
+}
---
+++ db/Dbinit.java
@@ -0,0 +1,36 @@
+package db;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Dbinit {
+    public static void init() {
+        File phone = new File("phone.txt");
+        if (!phone.exists()) {
+            try {
+                phone.createNewFile();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        File laptop = new File("laptop.txt");
+        if (!laptop.exists()) {
+            try {
+                laptop.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        File file = new File("tv.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
+}
